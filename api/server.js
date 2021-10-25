44dd1fe3e19c063b0f967101d7efd93425fca7bb
zach-morris-txt <zach-morris@lambdastudents.com>
2021-10-25 10:29:56 -0400

    Refactor2

--- api/server.js
+++ api/server.js
@@ -7,19 +7,6 @@ const authRouter = require('./api/auth/auth-router')
 const itemsRouter = require('./api/items/items-router')
 
 
-// const db = require('./data/db-config')
-
-// function getAllUsers() { return db('users') }
-
-// async function insertUser(user) {
-//   // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-//   // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-//   // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-//   const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-//   return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-// }
-
-
 //Instance Of Express App
 const server = express()
 
@@ -33,6 +20,14 @@ server.use(cors())
 //Consuming Routers
 server.use('/api/auth', authRouter);
 server.use('/api/items', itemsRouter);
+
+
+// "/" Endpoint
+server.get('/', (req, res, next) => {
+  res.status(200).json(
+    `My Landing Page`
+  )
+})
 
 
 //Error-Handling Middleware
